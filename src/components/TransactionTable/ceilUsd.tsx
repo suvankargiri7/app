9a8bd4b2b17317891c3e2e0eb7939217ac90bb7b
Suvankar Giri <suvankargiri7@gmail.com>
2023-08-19 18:41:06 +0530

    changed exchange api url

--- src/components/TransactionTable/ceilUsd.tsx
+++ src/components/TransactionTable/ceilUsd.tsx
@@ -10,7 +10,10 @@ const CeilUsd: FunctionComponent<ceilUsdProps> = (props) => {
   const [usdValue, setUsdValue] = useState<number>(0);
 
   const url =
-    "https://anyapi.io/api/v1/exchange/convert?apiKey=ua4h13q2p49nekpgd4d8n87sc2lnhfoud77k29p2o5143tggj9ioe";
+    //"https://anyapi.io/api/v1/exchange/rates?apiKey=ua4h13q2p49nekpgd4d8n87sc2lnhfoud77k29p2o5143tggj9io";
+
+    "http://api.exchangeratesapi.io/v1/convert?access_key=ea2dda9c9d413cd209735539b095224c&from=INR&to=USD&amount="+props.amount; 
+
 
   const fetchData = async () => {
       try {
